eb0848df59106b5db0444c393c5c80f670ae9e52
Marco Tuzza <marcotuzza@hotmail.it>
2020-06-10 16:50:25 +0200

    Grafici funzionanti

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var venditeMensili = {};
 
+var venditePersonali = {};
+
 $.ajax({
 
     "url": "http://157.230.17.132:4032/sales",
@@ -22,13 +24,32 @@ $.ajax({
 
             }
 
-        }
+            var venditore = sales[i].salesman
+
+
+            if (!venditePersonali.hasOwnProperty(venditore)) {
+
+                venditePersonali[venditore] = sales[i].amount;
+
+            } else {
+
+                venditePersonali[venditore] += sales[i].amount;
+
+            }
+
+        };
+
+        var venditori = Object.keys(venditePersonali)
+
+        var venditeSingole = Object.values(venditePersonali)
 
         var vendite = Object.values(venditeMensili)
 
-        var ctx = document.getElementById('barChart').getContext('2d');
+        var ctxB = document.getElementById('barChart').getContext('2d');
 
-        var myChart = new Chart(ctx, {
+        var ctxP = document.getElementById('pieChart').getContext('2d');
+
+        var myChart = new Chart(ctxB, {
             type: 'line',
             data: {
                 labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
@@ -36,12 +57,12 @@ $.ajax({
                     label: 'Andamento vendite',
                     data: vendite,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -50,6 +71,39 @@ $.ajax({
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
                         'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+
+        var myChart = new Chart(ctxP, {
+            type: 'pie',
+            data: {
+                labels: venditori,
+                datasets: [{
+                    label: 'Andamento vendite',
+                    data: venditeSingole,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
                     ],
                     borderWidth: 1
                 }]
